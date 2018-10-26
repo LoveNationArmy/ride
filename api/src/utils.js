b83b1ffe3dc3eb85f0ea89324a045553372709b5
George Stagas <gstagas@gmail.com>
2018-10-26 11:05:26 +0300

    split date and time

--- api/src/utils.js
+++ api/src/utils.js
@@ -1,8 +1,9 @@
 const slug = require('slug')
 
-// 2018-10-15-09-30/xania/soygia/hyundai-accent-xnz-3423
+// 2018-10-15/0930/xania/soygia/hyundai-accent-xnz-3423
 exports.generateOfferId = (offer) => [
   offer.date,
+  offer.time,
   offer.departure,
   offer.arrival,
   offer.vehicle
--- api/test/integration/routes/mutations.test.js
+++ api/test/integration/routes/mutations.test.js
@@ -4,19 +4,22 @@ const request = require('supertest')
 const fixtures = {
   offers: [
     {
-      date: '2018-10-15 09:30',
+      date: '2018-10-15',
+      time: '09:30',
       departure: 'Χανιά',
       arrival: 'Σούγια',
       vehicle: 'Hyundai Accent XNZ 3423'
     },
     {
-      date: '2018-10-15 12:30',
+      date: '2018-10-15',
+      time: '12:30',
       departure: 'Χανιά',
       arrival: 'Παλαιόχωρα',
       vehicle: 'Fiat Punto XNA 5657'
     },
     {
-      date: '2018-10-18 18:30',
+      date: '2018-10-18',
+      time: '18:30',
       departure: 'Σούγια',
       arrival: 'Χανιά',
       vehicle: 'Hyundai Accent XNZ 3423'
@@ -27,15 +30,15 @@ const fixtures = {
 const expected = {
   offers: [
     {
-      id: '2018-10-15-0930/xania/soygia/hyundai-accent-xnz-3423',
+      id: '2018-10-15/0930/xania/soygia/hyundai-accent-xnz-3423',
       ...fixtures.offers[0]
     },
     {
-      id: '2018-10-15-1230/xania/palaioxwra/fiat-punto-xna-5657',
+      id: '2018-10-15/1230/xania/palaioxwra/fiat-punto-xna-5657',
       ...fixtures.offers[1]
     },
     {
-      id: '2018-10-18-1830/soygia/xania/hyundai-accent-xnz-3423',
+      id: '2018-10-18/1830/soygia/xania/hyundai-accent-xnz-3423',
       ...fixtures.offers[2]
     },
   ]
