e7cc95333ab20286cee11431b534402bd30382c2
George Stagas <gstagas@gmail.com>
2018-11-18 10:11:15 +0200

    more wip wip web

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -8,8 +8,16 @@ export default (props) =>
   <div className='header'>
     <Logo />
     <div className='header-nav'>
-      <Icon onClick={() => props.mutations.setScreen('offers')} active={props.data.ui.screen === 'offers'} value='🚀' />
-      <Icon onClick={() => props.mutations.setScreen('requests')} active={props.data.ui.screen === 'requests'} value='⛱' />
+      <Icon
+        onClick={() => props.mutations.setScreen('offers')}
+        active={props.data.ui.screen === 'offers'}
+        value='🚀'
+      />
+      <Icon
+        onClick={() => props.mutations.setScreen('requests')}
+        active={props.data.ui.screen === 'requests'}
+        value='⛱'
+      />
     </div>
     <Login {...props} />
   </div>
